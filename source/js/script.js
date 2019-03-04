b86b5e367fa8b7964758b8cb732cdbf972c342fb
官全洪 <guanquanhong@163.com>
2019-03-04 17:53:02 +0800

    submit

---
+++ source/js/script.js
@@ -0,0 +1,7 @@
+(function () {
+  const navBtn = document.querySelector(".header-navbar-btn");
+  
+  navBtn.addEventListener("click", function () {
+    this.classList.toggle("active");
+  })
+})();

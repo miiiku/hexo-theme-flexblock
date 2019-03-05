c2f3a9f03ac580b30ee908dd6536fec3016f705b
官全洪 <guanquanhong@163.com>
2019-03-05 19:37:58 +0800

    submit

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,19 @@
 (function () {
   const navBtn = document.querySelector(".header-navbar-btn");
-  
+  const commentContainer = document.querySelector(".comment-container");
   navBtn.addEventListener("click", function () {
     this.classList.toggle("active");
   })
+  commentContainer && initComment();
 })();
+
+function initComment () {
+  new Valine({
+    el: ".comment-container",
+    appId: "oTmfgIaoB7PfPATvA9tsOwTu-gzGzoHsz",
+    appKey: "PPbJycuezKD3BJlhHUy8zOnG",
+    placeholder: "随便说点什么叭～",
+    notify: true,
+    visitor: true
+  })
+}

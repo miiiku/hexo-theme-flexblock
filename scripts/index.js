b35333cb129b16f5c670e982dbe51579aca245ca
官全洪 <guanquanhong@163.com>
2019-03-07 20:10:35 +0800

    submit

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,16 @@ hexo.extend.tag.register("dplayer", function(args) {
 
   return `<div class="dplayer" data-url="${url}" data-cover="${cover}"></div>`
 });
+
+/** 
+ * waterfall tag
+ * 
+ * Syntax:
+ *  {% waterfall %}
+ *  ![img](img)...
+ *  {% endwaterfall %}
+ */
+hexo.extend.tag.register("waterfall", function(args, content) {
+  const text = hexo.render.renderSync({ text: content, engine: 'markdown' });
+  return `<div class="waterfall-container">${text}</div>`;
+}, { ends: true });
---
+++ source/lib/initMacy.js
@@ -0,0 +1,53 @@
+(function () {
+  const selector = ".waterfall-container";
+  const macyContainer = document.querySelector(selector);
+  macyContainer && initMacy(selector);
+
+  function initMacy(selector) {
+    initCss();
+    new Macy({
+      container: selector,
+      trueOrder: false,
+      waitForImages: false,
+      useOwnImageLoader: false,
+      mobileFirst: true,
+      columns: 1,
+      margin: {
+          y: 16,
+          x: '2%',
+      },
+      breakAt: {
+          1200: {
+            margin: {
+              x: 20
+            },
+            columns: 4
+          },
+          940: {
+            margin: {
+              x: 20
+            },
+            columns: 3
+          },
+          520: {
+            margin: {
+              x: 10
+            },
+            columns: 2
+          },
+          400: {
+            margin: {
+              x: 0
+            },
+            columns: 1
+          },
+      }
+    });
+  }
+
+  function initCss() {
+    const cssStyle = document.createElement("style")
+    cssStyle.innerText = `.waterfall-container p { display: block; border-radius: 4px; overflow: hidden; }`
+    document.head.appendChild(cssStyle);
+  }
+})();

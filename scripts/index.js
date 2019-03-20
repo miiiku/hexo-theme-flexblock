7aeaf5e2482a882761e43b884f91165f3a417a4d
官全洪 <guanquanhong@163.com>
2019-03-20 18:40:00 +0800

    submit

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 * dplayer tag
 *
 * Syntax:
-*   {% dplayer url [cover] %}
+*   {% dplayer <url> [cover] %}
 */
 hexo.extend.tag.register("dplayer", function(args) {
   var url = args.shift(), cover = "";
@@ -26,3 +26,13 @@ hexo.extend.tag.register("waterfall", function(args, content) {
   const text = hexo.render.renderSync({ text: content, engine: 'markdown' });
   return `<div class="waterfall-container">${text}</div>`;
 }, { ends: true });
+
+hexo.extend.filter.register('before_post_render', function(data) {
+  if (!hexo.theme.config.permalink) return data
+  if (data.layout === "post") {
+    // console.log(hexo.theme.config.permalink)
+    // console.log(data.slug)
+    data.slug = data.date.format("YYYY-MM-DD-HH-mm-ss")
+    return data
+  }
+});

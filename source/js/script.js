67cab0197c9c5142d30a7be6234d73bb1257e015
官全洪 <guanquanhong@163.com>
2019-12-23 17:34:27 +0800

    修改代码

--- source/js/script.js
+++ source/js/script.js
@@ -5,21 +5,4 @@ window.addEventListener("load", function() {
   navBtn.addEventListener("click", function () {
     this.classList.toggle("active");
   });
-  
-  // Fancybox Caption
-  // $('.article-entry').each(function(i){
-  //   $(this).find('img').each(function(){
-  //     if ($(this).parent().hasClass('fancybox')) return;
-
-  //     var alt = this.alt;
-
-  //     if (alt && $(this).parents(".waterfall-container").length < 1) $(this).after('<span class="caption">' + alt + '</span>');
-
-  //     $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-  //   });
-
-  //   $(this).find('.fancybox').each(function(){
-  //     $(this).attr('rel', 'article' + i);
-  //   });
-  // });
 })
